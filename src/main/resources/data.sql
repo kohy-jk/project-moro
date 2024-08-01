cd6622055686b28110ca11ac85ca22cf3f4828f1
Jan Kohnheiser <jan.kohnheiser@level.systems>
2024-08-01 23:17:59 +0200

    FAZE-1 connect and init DB
    Change-Id: Ied1544cd538a9b4a8e0c20f113ac7a1709067c47

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+
+TRUNCATE TABLE users RESTART IDENTITY;
+
+INSERT INTO users (user_name) VALUES ('franta');
+INSERT INTO users (user_name) VALUES ('pepa');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    user_name text NOT NULL UNIQUE
+);
